0b9f90b4b7143e970a495ccacdbdce5ba8aca80f
loui9583 <loui9583@stud.kea.dk>
2022-03-14 18:08:37 +0100

    Formattering Room1

--- src/Room1.java
+++ src/Room1.java
@@ -1,18 +1,19 @@
 public class Room1 extends Rooms {
 
 
-  String east (){
-
-    currentRoom=new Room2();
-    return "";
-  }
-  String south (){
-    currentRoom=new Room4();
-    return "";
-  }
-
   Room1() {
-    Rooms.navn="Room 1";
+    Rooms.navn = "Room 1";
     System.out.println(navn);
+  }
+
+  String east() {
+
+    currentRoom = new Room2();
+    return "";
+  }
+
+  String south() {
+    currentRoom = new Room4();
+    return "";
   }
 }
